6f92d8e8c9d05c557dd0c6d766b95a4577f70e96
João Pedro Miranda Salim <joaopedromirandasalim@gmail.com>
2023-10-31 22:25:41 -0300

    Cria funções no index.js

--- index.js
+++ index.js
@@ -2,3 +2,12 @@ let joao = 1;
 let maria = 2;
 maria = joao + maria;
 joao = 0;
+
+function deJoaoParaMaria(){
+    maria = maria + joao;
+    joao = 0;
+}
+function deMariaParaJoao(){
+    joao = joao + maria;
+    maria = 0;
+}
